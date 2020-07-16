384230437b8023592d6e7ef6456b5b8d1fc30162
Hu2y <sanghun_@daum.net>
2020-07-16 17:14:50 +0900

    [infinite-scroll #15] Feat: infinite-scroll 구현
    - what: Main page infinite-scroll 구현
    - how: IntersectionObserver을 사용해서 cardlist의 마지막 card를 타겟으로 마지막 카드가 랜더링될때 다음 데이터를 fetch해서 다음 데이터를 추가해주었다.
    - why: 초기 랜더링 속도 개선 및  U.X개선

--- src/components/CardList/CardList.component.tsx
+++ src/components/CardList/CardList.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import Card from '#Custom/Card/Card.component';
 
@@ -7,11 +7,46 @@ import { Container } from './CardList.styles';
 import { CARD_DATA } from './CardList.data';
 
 const CardList = () => {
+  
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
+  const setRefTarget = (a: HTMLDivElement | null) => {
+    setTarget(a);
+  };
+  const [card, setCard] = useState(CARD_DATA)
+  const options = {
+    root: null,
+    threshold: 0.5,
+    rootMargin: '-100px -50px'
+  };
+
+  useEffect(() => {
+    const handleIntersection = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        }
+        observer.unobserve(entry.target);
+        fetch('http://localhost:8080/todo')
+          .then((res) => res.json())
+          .then((res) => setCard([...card,...res]))
+      });
+    };
+
+    const io = new IntersectionObserver(handleIntersection, options);
+    if (target) {
+      io.observe(target);
+    }
+    return () => io.disconnect();
+  }, [target]);
+
   return (
     <Container>
-      {CARD_DATA.map((cardData, index) => (
-        <Card {...cardData} key={index} />
-      ))}
+      {
+        card.map((cardData, index) => {
+          const lastElement = index === card.length - 1;
+          return <Card {...cardData} key={index} refCard={lastElement ? setRefTarget : null} />
+        })
+      }
     </Container>
   );
 };
