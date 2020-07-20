0fcb057aa1b9c015a9953eb2fef9d6b96ce3f95d
Hu2y <sanghun_@daum.net>
2020-07-20 17:31:22 +0900

    [Posts Page #21] Feat: 블로그 읽기 Page 작성
    - what:  Posts Page을 작성했다.
    - how: react-router-dom을 이용하여, 동적으로 routing을 처리했다.
    - why: 포스팅마다 동적으로 가지는 값을 라우팅처리해서 랜더링 해주기위해서 사용했다.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Main from '@Pages/Main/Main.component';
 import Search from '@Pages/Search/Search.component';
+import Posts from '@Pages/Posts/Posts.component';
 import Header from '#/Header/Header.component';
 
 import GlobalStyle from '@Styles/GlobalStyle';
@@ -13,8 +14,9 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route path="/" component={Main} exact />
+        <Route exact path="/" component={Main} />
         <Route path="/search" component={Search} />
+        <Route path="/posts/:id" component={Posts} />
       </Switch>
     </>
   );
