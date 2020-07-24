6412f5f246953c93d12684a52b7ab0a97031d081
Hu2y <sanghun_@daum.net>
2020-07-25 02:16:16 +0900

    [Modal, Portal #38] Feat: 공용으로 사용될 Modal, Portal 컴포넌트 작성
    - what: 공용으로 사용하게 될 Modal Component와 Portal Component를 작성하였다
    - how: 글을 작성하여 버튼을 누르게 되면 Modal창이 띄워지도록 하였고, 모달창이 띄워질때 바깥영역의 스크롤이 작동하지 않도록 적용했다, 또한 모달창에 Portal을 적용하였다.
    - why: 다른부분에서 모달창이 또 쓰일수 있기때문에 재사용성을 위해 Children으로 Props을 넘겨 공용으로 쓸수있게 만들었고, Potal을 Modal에 입힐 경우 해당 DOM의 위치에 따라 성능에 영향을 받기 때문에 DOM 의 트리구조에 종속되지 않고 원하는 곳에 컴포넌트를 렌더링시킬수 있도록 하였다.

---
+++ src/components/Common/Portal/Portal.component.tsx
@@ -0,0 +1,18 @@
+import { useMemo } from 'react'
+import { createPortal } from 'react-dom'
+import { render } from '@testing-library/react'
+
+interface IPotalProps {
+  children: any;
+  elementId: any;
+}
+
+const Portal = ({ children, elementId }: IPotalProps) => {
+  const rootElement: any = useMemo(() => document.getElementById(elementId), [
+    elementId,
+  ])
+  
+  return createPortal(children, rootElement)
+}
+
+export default Portal
