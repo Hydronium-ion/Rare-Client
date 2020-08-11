df94ecd90b7b2dd2964fc9f5f75beaf3325da2f0
sangwon21 <junothyme@gmail.com>
2020-08-11 16:30:24 +0900

    [Skeleton U.I. Animation #58] Style: Skeleton U.I.에 animation 적용
    - what: Skeleton U.I.에 animatoin을 적용했다.
    - how: 화면 크기에 따라 다른 animation을 적용했다.
    - why: 사용자가 로딩 상태를 인식할 수 있는 U.I.가 필요했다. 기본 Skeleton U.I.에 animation을 적용시켜 무언가 진행이 되는 듯한 인상을 줄 수 있는 U.I.를 만들었다.

--- src/components/Custom/Card/SkeletonCard.styles.tsx
+++ src/components/Custom/Card/SkeletonCard.styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Skeleton = styled.div`
   background-repeat: no-repeat;
@@ -15,12 +15,25 @@ export const Skeleton = styled.div`
   }
 
   @media (max-width: ${({ theme }) => theme.sizes.md}) {
-    background-image: linear-gradient(lightgray 30%, transparent 0), linear-gradient(lightgray 95%, transparent 0),
+    background-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 40%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      linear-gradient(lightgray 30%, transparent 0), linear-gradient(lightgray 95%, transparent 0),
       linear-gradient(lightgray 100%, transparent 0),
       radial-gradient(circle 28px at 10% 39%, lightgray 99%, transparent 0),
       linear-gradient(lightgray 30%, transparent 0), linear-gradient(lightgray 30%, transparent 0);
-    background-position: 0 0, 0 12%, 0 40%, 0 70%, 40% 73%, 85% 73%;
-    background-size: 20% 20%, 90% 15%, 90% 25%, 60% 15%, 25% 15%, 25% 15%;
+    background-position: 0 0, 0 0, 0 12%, 0 40%, 0 70%, 40% 73%, 85% 73%;
+    background-size: 20% 80%, 20% 20%, 90% 15%, 90% 25%, 60% 15%, 25% 15%, 25% 15%;
+
+    animation: shineLMd 1s infinite;
+    @keyframes shineLMd {
+      to {
+        background-position: 100% 0, 0 0, 0 12%, 0 40%, 0 70%, 40% 73%, 85% 73%;
+      }
+    }
   }
 
   @media (min-width: ${({ theme }) => theme.sizes.md}) {
@@ -28,12 +41,26 @@ export const Skeleton = styled.div`
     height: 500px;
     margin: 0 0 2rem calc((100% - (39vw * 2)) / 3);
     background-repeat: no-repeat;
-    background-image: linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
+    background-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 40%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
       linear-gradient(lightgray 100%, transparent 0),
       radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
       linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
-    background-position: 0 0, 0 9%, 0 30%, 0 55%, 40% 63%, 85% 63%;
-    background-size: 20% 20%, 90% 15%, 90% 21%, 60% 13%, 25% 20%, 25% 20%;
+    background-position: 0 0, 0 0, 0 9%, 0 30%, 0 55%, 40% 63%, 85% 63%;
+    background-size: 20% 80%, 20% 20%, 90% 15%, 90% 21%, 60% 13%, 25% 20%, 25% 20%;
+
+    animation: shineMd 1s infinite;
+
+    @keyframes shineMd {
+      to {
+        background-position: 100% 0, 0 0, 0 9%, 0 30%, 0 55%, 40% 63%, 85% 63%;
+      }
+    }
   }
 
   @media (min-width: ${({ theme }) => theme.sizes.lg}) {
@@ -41,12 +68,26 @@ export const Skeleton = styled.div`
     width: 30%;
     height: 500px;
     background-repeat: no-repeat;
-    background-image: linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
+    background-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 40%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
       linear-gradient(lightgray 100%, transparent 0),
       radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
       linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
-    background-position: 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
-    background-size: 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
+    background-position: 0 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
+    background-size: 20% 50%, 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
+
+    animation: shineLg 1s infinite;
+
+    @keyframes shineLg {
+      to {
+        background-position: 100% 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
+      }
+    }
   }
 
   @media (min-width: ${({ theme }) => theme.sizes.xl}) {
@@ -54,12 +95,26 @@ export const Skeleton = styled.div`
     width: 24%;
     height: 500px;
     background-repeat: no-repeat;
-    background-image: linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
+    background-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 40%,
+        rgba(255, 255, 255, 0) 70%
+      ),
+      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
       linear-gradient(lightgray 100%, transparent 0),
       radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
       linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
-    background-position: 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
-    background-size: 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
+    background-position: 0 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
+    background-size: 20% 50%, 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
+
+    animation: shineXl 1s infinite;
+
+    @keyframes shineXl {
+      to {
+        background-position: 100% 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
+      }
+    }
   }
 `;
 
