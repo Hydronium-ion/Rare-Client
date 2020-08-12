e4e6dbba6ba3fd238f0b2292ef67dd8be6a02cdc
sangwon21 <junothyme@gmail.com>
2020-08-12 19:19:18 +0900

    [Post Skeleton U.I. #61] Style: Post Skeleton U.I. 작성
    - what: Post가 loading 상태일 때, 보여주는 Skeleton U.I를 작성했다.
    - how:
    - why: 사용자 U.X.개선을 위하여, Skeleton U.I.를 작성했다.

--- src/components/Custom/Card/SkeletonCard.styles.tsx
+++ src/components/Custom/Card/SkeletonCard.styles.tsx
@@ -2,10 +2,9 @@ import styled, { keyframes } from 'styled-components';
 
 export const Skeleton = styled.div`
   background-repeat: no-repeat;
-  background-image: linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
-    linear-gradient(lightgray 100%, transparent 0),
-    radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0), linear-gradient(lightgray 20%, transparent 0),
-    linear-gradient(lightgray 20%, transparent 0);
+  background-image: linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 80%, transparent 0),
+    linear-gradient(#f0f0f0 100%, transparent 0), radial-gradient(circle 20px at 15% 35%, #f0f0f0 99%, transparent 0),
+    linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 20%, transparent 0);
   background-position: 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
   background-size: 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
 
@@ -21,10 +20,9 @@ export const Skeleton = styled.div`
         rgba(255, 255, 255, 0.5) 40%,
         rgba(255, 255, 255, 0) 70%
       ),
-      linear-gradient(lightgray 30%, transparent 0), linear-gradient(lightgray 95%, transparent 0),
-      linear-gradient(lightgray 100%, transparent 0),
-      radial-gradient(circle 28px at 10% 39%, lightgray 99%, transparent 0),
-      linear-gradient(lightgray 30%, transparent 0), linear-gradient(lightgray 30%, transparent 0);
+      linear-gradient(#f0f0f0 30%, transparent 0), linear-gradient(#f0f0f0 95%, transparent 0),
+      linear-gradient(#f0f0f0 100%, transparent 0), radial-gradient(circle 28px at 10% 39%, #f0f0f0 99%, transparent 0),
+      linear-gradient(#f0f0f0 30%, transparent 0), linear-gradient(#f0f0f0 30%, transparent 0);
     background-position: 0 0, 0 0, 0 12%, 0 40%, 0 70%, 40% 73%, 85% 73%;
     background-size: 20% 80%, 20% 20%, 90% 15%, 90% 25%, 60% 15%, 25% 15%, 25% 15%;
 
@@ -47,10 +45,9 @@ export const Skeleton = styled.div`
         rgba(255, 255, 255, 0.5) 40%,
         rgba(255, 255, 255, 0) 70%
       ),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
-      linear-gradient(lightgray 100%, transparent 0),
-      radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 80%, transparent 0),
+      linear-gradient(#f0f0f0 100%, transparent 0), radial-gradient(circle 20px at 15% 35%, #f0f0f0 99%, transparent 0),
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 20%, transparent 0);
     background-position: 0 0, 0 0, 0 9%, 0 30%, 0 55%, 40% 63%, 85% 63%;
     background-size: 20% 80%, 20% 20%, 90% 15%, 90% 21%, 60% 13%, 25% 20%, 25% 20%;
 
@@ -74,10 +71,9 @@ export const Skeleton = styled.div`
         rgba(255, 255, 255, 0.5) 40%,
         rgba(255, 255, 255, 0) 70%
       ),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
-      linear-gradient(lightgray 100%, transparent 0),
-      radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 80%, transparent 0),
+      linear-gradient(#f0f0f0 100%, transparent 0), radial-gradient(circle 20px at 15% 35%, #f0f0f0 99%, transparent 0),
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 20%, transparent 0);
     background-position: 0 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
     background-size: 20% 50%, 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
 
@@ -101,10 +97,9 @@ export const Skeleton = styled.div`
         rgba(255, 255, 255, 0.5) 40%,
         rgba(255, 255, 255, 0) 70%
       ),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 80%, transparent 0),
-      linear-gradient(lightgray 100%, transparent 0),
-      radial-gradient(circle 20px at 15% 35%, lightgray 99%, transparent 0),
-      linear-gradient(lightgray 20%, transparent 0), linear-gradient(lightgray 20%, transparent 0);
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 80%, transparent 0),
+      linear-gradient(#f0f0f0 100%, transparent 0), radial-gradient(circle 20px at 15% 35%, #f0f0f0 99%, transparent 0),
+      linear-gradient(#f0f0f0 20%, transparent 0), linear-gradient(#f0f0f0 20%, transparent 0);
     background-position: 0 0, 0 0, 0 8%, 0 25%, 0 45%, 40% 50%, 85% 50%;
     background-size: 20% 50%, 20% 20%, 90% 15%, 90% 15%, 60% 15%, 25% 15%, 25% 15%;
 
